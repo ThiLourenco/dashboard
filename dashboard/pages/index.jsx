b86487b892add30ef85e450dc07c5fc11d577325
Thigo <thi.it.solutions@gmail.com>
2023-05-28 13:00:30 -0300

    chore(): creating connection with backend

--- dashboard/pages/index.jsx
+++ dashboard/pages/index.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import client from "src/sanity";
 import Heading from "src/UI/Heading/Heading";
 import Cards from "src/components/Card/Cards";
 import SalesGoal from "src/components/SalesGoal/SalesGoal";
@@ -6,7 +7,7 @@ import SalesHistory from "src/components/SalesHistory/SalesHistory";
 import TopSales from "src/components/TopSales/TopSales";
 import styles from "styles/Dashboard.module.scss";
 
-export default function Home() {
+export default function Home({ orders, products, config }) {
   return (
     <section className={styles.dashboard}>
       <Heading 
@@ -28,5 +29,19 @@ export default function Home() {
         </section>
       </section>
     </section>
-  )
+  );
+}
+
+export const getStaticProps = async () => {
+  const orders = await client.fetch('*[_type == "orders"]');
+  const products = await client.fetch('*[_type == "products"]');
+  const config = await client.fetch('*[_type == "config"]');
+
+  return {
+    props: {
+      orders,
+      products,
+      config,
+    }
+  }
 }
