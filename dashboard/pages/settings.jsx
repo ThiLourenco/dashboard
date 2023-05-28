91918a482115ae7208adce96b7f25205f1962e52
Thigo <thi.it.solutions@gmail.com>
2023-05-28 11:46:15 -0300

    chore(): create settings pages

--- dashboard/pages/settings.jsx
+++ dashboard/pages/settings.jsx
@@ -3,7 +3,32 @@ import Heading from "src/UI/Heading/Heading";
 const Settings = () => {
   return (
     <section>
-      <Heading title='Configurações' subtitle='Minhas Configurações' />
+      <Heading title='Configurações' subtitle='Defina as configurações da aplicação' />
+      <form>
+      <div>
+          <label htmlFor="name" className="hidden">
+            Nome
+          </label>
+          <input 
+            type="text" 
+            name="name"
+            id="name"
+            placeholder="Nome da empresa"
+          />
+        </div>
+        <div>
+          <label htmlFor="price" className="hidden">
+            Meta de Vendas
+          </label>
+          <input 
+            type="number" 
+            name="price"
+            id="price"
+            placeholder="Meta de vendas"
+          />
+        </div>
+        <button className="btn btn-primary">Salvar</button>
+        </form>
     </section>
   )
 }
