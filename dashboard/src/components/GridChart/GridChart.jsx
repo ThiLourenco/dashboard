cb22b3e6819a753cf6521a5f7248351b001f4477
Thigo <thi.it.solutions@gmail.com>
2023-06-08 19:34:35 -0300

    chore(): handle product table

--- dashboard/src/components/GridChart/GridChart.jsx
+++ dashboard/src/components/GridChart/GridChart.jsx
@@ -1,10 +1,26 @@
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css'
+
 
 const GridChart = ({ columns, data }) => {
-  const customizeCell = (args) => {
-    if (args.columns.field === 'image' ) {
-      args.cell.querySelector('img').setAttribute('src', args.data.image);
-    }
-  }
+  // const customizeCell = (args) => {
+  //   if (args.columns.field === 'image' ) {
+  //     args.cell.querySelector('img').setAttribute('src', args.data.image);
+  //   }
+  // }
+
+  const dataList = [
+    { id: 1, name: 'a', email: 'a@email.com', avartar: '...' },
+    { id: 2, name: 'b', email: 'b@email.com', avartar: '...' },
+    { id: 3, name: 'c', email: 'c@email.com', avartar: '...' }
+  ];
+  
+  // const ImageCell = ({ rowData, dataKey, ...rest }) => (
+  //   <Cell {...rest}>
+  //     <img src={rowData[dataKey]} width="50" />
+  //   </Cell>
+  // );
+  
 
   const renderColumns = columns.map((column, i) => (
     // <ColumnDirective key={i} {...column}></ColumnDirective>
@@ -12,9 +28,32 @@ const GridChart = ({ columns, data }) => {
   ))
 
   return (
-    <div>
-     oi
-    </div>
+    <Table data={dataList}>
+    <Column width={100} sortable fixed resizable>
+      <HeaderCell>ID</HeaderCell>
+      <Cell dataKey="id" />
+    </Column>
+
+    <Column width={100} sortable resizable>
+      <HeaderCell>Name</HeaderCell>
+      <Cell dataKey="name" />
+    </Column>
+
+    <Column width={100} sortable resizable>
+      <HeaderCell>Email</HeaderCell>
+      <Cell>
+        Email
+      </Cell>
+    </Column>
+
+    <Column width={100} resizable>
+      <HeaderCell>Avartar</HeaderCell>
+      {/* <ImageCell dataKey="avartar" /> */}
+      <Cell>
+        Image
+      </Cell>
+    </Column>
+  </Table>
   );
 };
 
